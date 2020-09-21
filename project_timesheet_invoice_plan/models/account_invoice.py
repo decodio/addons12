a9148136d1c0ab47ed71908e932c9675900eaa26
dario <dario.meniss@slobodni-programi.hr>
2020-09-21 10:50:48 +0200

    store returned to false, changed name of timesheet_ids field no to overlap with other modules

--- project_timesheet_invoice_plan/models/account_invoice.py
+++ project_timesheet_invoice_plan/models/account_invoice.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    timesheet_ids = fields.One2many(
+    project_timesheet_ids = fields.One2many(
         comodel_name='account.analytic.line',
         inverse_name='invoice_id',
         string='Timesheet activities')
