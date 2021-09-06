9e7620f402b126322ea8eedeeb2d9520d7a0cf72
andro <andro.radic@ecodica.eu>
2021-09-06 12:26:50 +0200

    [10011061;Ecodica;IMP] Renames "decodio" to "ecodica" in all manifest files.

--- account_invoice_tax_note_view/__manifest__.py
+++ account_invoice_tax_note_view/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2021 Ecodica
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Account invoice tax note view",
     'version': "12.0.1.0.0",
-    "author": "Decodio Applications,Odoo Community Association (OCA)",
+    "author": "Ecodica, Odoo Community Association (OCA)",
     "development_status": "Production/Stable",
     "summary": "Adding missing filed on view when OCA account_invoice_tax_note """
                "and account_group_menu are installed",
